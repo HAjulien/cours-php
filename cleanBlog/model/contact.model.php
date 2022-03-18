6d31028c13f86bbf05937e4dcac01b2074c5f538
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-18 12:31:29 +0100

    Ajout 4 models

---
+++ cleanBlog/model/contact.model.php
@@ -0,0 +1,5 @@
+<?php
+    $bg='assets/img/contact-bg.jpg';
+    $title='Nous contacter';
+    $subtitle='Pour nous contacter';
+
--- cleanBlog/model/home.model.php
+++ cleanBlog/model/home.model.php
@@ -1,4 +1,9 @@
 <?php
-$title="Blog de surf";
-$subtitle="Le meilleur sport de glisse";
+    $bg='assets/img/home-bg.jpg';
+    $title='Blog de surf';
+    $subtitle='Le meilleur sport de glisse';
 
+    $title_art_1='La planche de surf ';
+    $subtitle_art_1='Comment bien la choisir pour progresser';
+    $author_art_1='Phillipe G.';
+    $date_art_1='le 17 Mars 2022';
