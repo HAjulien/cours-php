34324eae3836d60df2eb5a4c796937c69cdd45d3
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-28 09:55:30 +0200

    ok

--- dashboard/exports/requetes.sql
+++
@@ -1,8 +0,0 @@
--- page home
-
-SELECT cat_descr ,post_content ,post_title , display_name , post_date
-FROM blog_posts
-INNER JOIN blog_users
-ON post_author = ID
-INNER JOIN blog_categories
-ON post_category = cat_id
--- dashboard/view/home.view.php
+++
@@ -1,3 +0,0 @@
-<?php include('view/layout/nav.inc.php'); ?> 
-
-<?php include('view/layout/header.inc.php'); ?> 
